4e7703c72364aa4328764831457e45b8ee6209ee
Alyssa Slayton <ajs41@missouristate.edu>
2018-09-05 12:40:32 -0500

    added notes from 9/05

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
+#include <cstdlib>
 #include <iostream>
+#include "IntCell.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    IntCell cell1;
+    IntCell *cell_ptr;
+
+    cell_ptr = new IntCell{ 0 };
+    cell_ptr->write(5);
+    std::cout << "Cell contents: " << cell_ptr->read() << std::endl;
+
+    //delete everything created using new because it causes memory leaks
+    //don't exactly understand pointers, look it up
+    delete cell_ptr;
+    return EXIT_SUCCESS;
 
 }
